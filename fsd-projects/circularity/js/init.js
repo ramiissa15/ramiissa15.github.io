5daaf818c1f59c2121659486cd47289739e9e82e
ramiissa15 <issa522@wearescpps.org>
2025-11-07 16:25:43 +0000

    pushing circularity with error

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -35,9 +35,9 @@ var init = function (window) {
     // drawCircle();
     // drawCircle();
     // TODO 7 : Use a loop to create multiple circles
-for (var i = 0; i < 25; i++) {
-drawCircle()
-}
+    for (var i = 0; i < 25; i++) {
+      drawCircle();
+    }
     ///////////////////
     // PROGRAM LOGIC //
     ///////////////////
@@ -49,23 +49,23 @@ drawCircle()
         */
     function update() {
       // TODO 4 : Update the position of each circle using physikz.updatePosition()
-// physikz.updatePosition(circles[0]);
-// physikz.updatePosition(circles[1]);
-// physikz.updatePosition(circles[2]);
-// physikz.updatePosition(circles[3]);
-// physikz.updatePosition(circles[4]);
+      // physikz.updatePosition(circles[0]);
+      // physikz.updatePosition(circles[1]);
+      // physikz.updatePosition(circles[2]);
+      // physikz.updatePosition(circles[3]);
+      // physikz.updatePosition(circles[4]);
       // TODO 5 : Call game.checkCirclePosition() on your circles
-// game.checkCirclePosition(circles[0]);
-// game.checkCirclePosition(circles[1]);
-// game.checkCirclePosition(circles[2]);
-// game.checkCirclePosition(circles[3]);
-// game.checkCirclePosition(circles[4]);
+      // game.checkCirclePosition(circles[0]);
+      // game.checkCirclePosition(circles[1]);
+      // game.checkCirclePosition(circles[2]);
+      // game.checkCirclePosition(circles[3]);
+      // game.checkCirclePosition(circles[4]);
       // TODO 8 / TODO 9 : Iterate over the array
-   for (var i = 0; i < circles.length; i++) {
-    physikz.updatePosition(circles[i]);
-    game.checkCirclePosition(circles[i]);
-   } 
-
+      for (var i = 0; i < circles.length; i++) {
+        physikz.updatePosition(circles[i]);
+        game.checkCirclePosition(circles[i]);
+      }
+    }
     /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
@@ -78,15 +78,15 @@ drawCircle()
       }
 
       // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-if (circle.x < 0){
-  circle.x = canvas.width;
-}
-if (circle.y > canvas.height) {
-  circle.y = 0;
-}
-if (circle.y < 0) {
-  circle.y = canvas.height;
-}
+      if (circle.x < 0) {
+        circle.x = canvas.width;
+      }
+      if (circle.y > canvas.height) {
+        circle.y = 0;
+      }
+      if (circle.y < 0) {
+        circle.y = canvas.height;
+      }
       // YOUR TODO 6 CODE ENDS HERE //////////////////////////
     };
 
