5808bb77db9cd60d40dc862237e9108f9a63a215
ramiissa15 <issa522@wearescpps.org>
2025-12-16 14:57:36 +0000

    problem with Runtime

--- fsd-projects/runtime/js/view/background.js
+++ fsd-projects/runtime/js/view/background.js
@@ -50,6 +50,11 @@ moon.y = 100;
 moon.scaleX = 3.0;
 moon.scaleY = 3.0;
 background.addChild(moon);
+
+var circle = draw.circle(10, "white", "LightGray", 2);
+circle.x = canvasWidth * Math.random();
+circle.y = groundY * Math.random();
+background.addChild(circle);
             
             // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
             for (var i = 0; i < 5; ++i) {
